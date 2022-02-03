a71d7de4c51bf15a54c092f90d4e933eda591062
All1n524 <alenhung0524@gmail.com>
2022-02-04 00:30:15 +0800

    my file

--- Conversion03/SortingAnArray/SortingAnArray.cpp
+++ Conversion03/SortingAnArray/SortingAnArray.cpp
@@ -1,31 +1,21 @@
-//Sorting Sorting An Array 100 pts Allen Hung
+//Sorting SortingArray 100pts Allen Hung
 #include <iostream>
 using namespace std;
+ 
 int main()
 {
-    srand(time(NULL));
-    int arr[10];
-    int size = sizeof(arr)/sizeof(arr[0]);
-    cout <<"Before: ";
-    for (int i=0, i<size; i++)
-    {
-        cout <<arr[i] << "  ";
-
-    }
-    
-}
+    cout << "Before: 45, 87, 39, 32, 93, 86, 12, 44, 75, 50 " << endl;
+    int arr[] = { 45, 87, 39, 32, 93, 86, 12, 44, 75, 50 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+ 
+    sort(arr, arr + n, greater<int>());
+    reverse(arr, arr + n);
 
 
-//PRINTING 
-
-#include <iostream>
-using namespace std;
-int main(){
-// int luckyNumbers[6];
-int luckyNumbers[] = {4, 8, 15, 16, 23, 42};
-//         indexes:    0  1  2   3   4   5
-luckyNumbers[0] = 90;
-cout << luckyNumbers[0] << endl;
-cout << luckyNumbers[1] << endl;
-return 0;
+ 
+    cout << "After: ";
+    for (int i = 0; i < n; ++i)
+        cout << arr[i] << " ";
+ 
+    return 0;
 }
