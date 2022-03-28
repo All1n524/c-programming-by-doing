050cdcd5b2e3dc19c8ea6043dd78cbaa81df9216
All1n524 <alenhung0524@gmail.com>
2022-03-28 13:43:12 +0800

    my file

--- ClassAndObject4/ClassAndObject4.cpp
+++ ClassAndObject4/ClassAndObject4.cpp
@@ -1,109 +1,95 @@
-
 #include <iostream>
-#include <string>
+#include <ctime>
+#include <cmath>
 using namespace std;
 
-class Moon{
-    private:
-    string name; //name of moon
-    double distance; //distance from the planet it is the moon of
-    double mass; // mass of moon
-    double diameter; //diameter of moon
-
-    public:
-    Moon () {};
-
-    Moon (string n, double d, double m, double dia){ //constructor
-        name = n;
-        distance = d;
-        mass = m;
-        diameter = dia;
+class Moon
+{
+private:
+    string name;
+    double distance, mass, diameter;
+public:
+//default constructor 
+    Moon(){}
+    Moon(string mName, double mDistance, double mMass, double mDiameter)
+    {
+        name = mName;
+        distance = mDistance;
+        mass = mMass;
+        diameter = mDiameter;
     }
-
     string getName(){
         return name;
     }
-
-    double getDistance(){
+};
+class Planet
+{
+private:
+    string name;
+    double distance, mass, diameter;
+    int numMoons;
+    Moon TopMoons[3];
+public:
+   // Planet(){}
+    Planet(string pName, double pDistance, double pMass, double pDiameter, int pNumMoons, Moon pTM1, Moon pTM2, Moon pTM3)
+    {
+        name = pName;
+        distance = pDistance;
+        mass = pMass;
+        diameter = pDiameter;
+        numMoons = pNumMoons;
+        TopMoons[0] = pTM1;
+        TopMoons[1] = pTM2;
+        TopMoons[2] = pTM3;
+    }
+    double getDistance()
+    {
         return distance;
     }
-
-    double getMass(){
-        return mass;
+    Moon getMoon(int i)
+    {
+        return TopMoons[i];
     }
-
-    double getDiameter(){
-        return diameter;
-    }
-
-
-
-};
-
-class Planet{
-    private:
-    string name; //name of planet
-    double distance; //distance from sun
-    double mass; //mass of planet
-    double diameter; // diameter of planet
-    int numMoons; //number of moons
-    Moon TopMoons[3] ; //top 3 moons of the planet
-
-    public:
-    Planet () {};
-
-    Planet(string n, double d, double m, double dia, int mn){
-        name = n;
-        distance = d;
-        mass = m;
-        diameter = dia;
-        numMoons = mn;
-    }
-
-    void setMoons(Moon a, Moon b, Moon c){
-        TopMoons[0] = a;
-        TopMoons[1] = b;    
-        TopMoons[2] = c;
-    }
-
-    string getName (){
+    string getName()
+    {
         return name;
     }
-
-    double getDistance (){
-        return distance;
-    }
-
-    double getMass(){
-        return mass;
-    }
-
-    double getDiameter(){
-        return diameter;
-    }
-
-    double getNMoons(){
-        return numMoons;
-    }
 };
 
-int main (){
-    Moon moonlist [] = {
-        //Moon (string name, double distance, double mass(10^15 kg), double diameter)
-        Moon ("Phobos", 11.4, 10.6, 9.1*2),
-        Moon ("Deimos", 6.0, 2.4, 5.1*2),
-        
+double distanceP(Planet a, Planet b){
+    return abs(a.getDistance()-b.getDistance());
+}
+
+string findMoon(Moon x, Planet arr[8]){
+    for(int i=0; i<8; i++)
+    {
+        for(int j=0; j<3; j++)
+        {
+            if(x.getName() == (arr[i].getMoon(j)).getName())
+                return arr[i].getName();
+        }
+    }    
+    
+    
+   // return name;
+}
+int main()
+{
+    //Planet solarSystem[2] = {1,2};
+    Planet solarsystem[8] = {
+        Planet("Mercury", 69.592, 0.33, 4879.4, 0, Moon("No Moon", 0, 0, 0), Moon("No Moon", 0, 0, 0), Moon("No Moon", 0, 0 ,0)),
+        Planet("Jupiter", 745.39, 1998.13, 193820, 79, Moon("Io", 421600, 3629, 893.2), Moon("Europa", 670900, 3126, 480), Moon("Callisto", 1883000, 4800 ,1075)),
+        Planet("Venus",4.87,12104,108.2,0, Moon("NO MOON", 0, 0, 0), Moon("NO MOON", 0, 0, 0), Moon("NO MOON", 0, 0, 0)),
+        Planet("Earth",5.97,12756,149.6,1, Moon("Moon",378000,0.07346,3476.2), Moon("NO MOON", 0, 0, 0), Moon("NO MOON", 0, 0, 0)),
+        Planet("Mars",0.642,6792,227.9,2, Moon("Phobos",9270,22.5,10.6), Moon("Deimos", 23460,12.4,2.4), Moon("NO MOON", 0, 0, 0)),
+        Planet("Saturn",568,120536,1433.5,82, Moon("Titan",1221850,5150,1345.5), Moon("Mimas",185520,398,0.379), Moon("Rhea",527040,1528,23.1)),
+        Planet("Uranus",86.8,51118,2872.5,27, Moon("Umbriel",265970,1190,12.2), Moon("Ariel",191240,1160,12.9), Moon("Miranda",129780,472,0.66)),
+        Planet("Neptune",102,49528,4495.1,14, Moon("Neso",47279670,60,0.002), Moon("Sao",22337190,38,0.001), Moon("Naiad",48200,50,0.002))
     };
 
+    cout<<"Distance are: " <<distanceP(solarsystem[0],solarsystem[1]) <<endl;
 
-    Planet solarSystem [] = {
-        //Planet(string n, double d, double m, double dia, int mn)
-        Planet ("Mercury", 46.29, 0.33011, 4879, 0) //10^24 kg
-
-    };
-
-
-
+    cout<<"Find the planet Moon Europa is "<<findMoon(Moon("Europa", 670900, 3126, 480), solarsystem)<<endl;
 
     return 0;
 }
