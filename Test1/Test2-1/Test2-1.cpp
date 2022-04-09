4c961246bd9f974a0b171b7e927c9606650bfbea
All1n524 <alenhung0524@gmail.com>
2022-04-09 23:08:28 +0800

    my file

---
+++ Test1/Test2-1/Test2-1.cpp
@@ -0,0 +1,35 @@
+//Test 1: Sort 2D Array 2-1
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+    srand(time(NULL));
+    int c, r;
+    cout<<"Type in the column: ";
+    cin>>c;
+    cout<<endl;
+    cout<<"Type in the row: ";
+    cin>>r;
+    cout<<endl;
+
+    char a[c][r];
+    for (int i=0; i<c; i++)
+    {
+        for(int j=0; j<r; j++)
+        {
+            int x = rand()%25+'a';
+            char ch = (char)x;
+            a[i][j] = ch;
+            cout<<a[i][j]<< " ";
+        }
+
+    cout << "\n";
+    }
+    return 0;
+}
+
+
